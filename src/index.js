e69017434de9526ecf6420ccb6b122d04b64bf7a
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-06-21 16:56:18 +0530

    Added Routes for Explare and actor details

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import configureStore from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/_index.css';
 import App from './app';
-import {MovieDetailsPage} from './scenes/MovieDetailsPage';
+import { MovieDetailsPage } from './scenes/MovieDetailsPage';
+import { ActorDetails } from './scenes/ActorDetails/ActorDetails';
 
 ReactDOM.render(
 	<Provider store={configureStore()}>
 		<Router>
-			<Route exact path="/" component={App}/>
+			<Route exact path="/" component={App} />
 			<Route path="/movie/:id" component={MovieDetailsPage} />
+			<Route path="/actor/:id" component={ActorDetails} />
 		</Router>
 	</Provider>,
 	document.getElementById('root')
