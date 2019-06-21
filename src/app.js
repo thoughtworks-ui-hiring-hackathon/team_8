cde9d401f1f6470dafcaea21ddace8fd2d377cb6
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-06-21 11:06:15 +0530

    removed scaffolding page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import logo from './logo.svg';
 import './scss/style.scss';
 import {simpleAction} from './actions/simple-action';
 
@@ -12,16 +11,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo"/>
-					<h1 className="App-title">
-						Welcome to ThoughtWorks UI Dev Hiring Hackathon
-					</h1>
-					<pre>
-					  {JSON.stringify(this.props.simpleReducer)}
-					</pre>
-					<button onClick={this.simpleAction}>Test Sample Redux Action</button>
-				</header>
+				
 			</div>
 		);
 	}
