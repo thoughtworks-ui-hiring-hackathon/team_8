1bffa2b742903007a36b14bf76e877f81768a26f
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-06-21 17:06:00 +0530

    explore page search comboBOx

--- src/scenes/Explore/Explore.js
+++ src/scenes/Explore/Explore.js
@@ -1,10 +1,26 @@
-import React from 'react'
-
-
+import React from 'react';
+import { Row, Col } from 'reactstrap';
+import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
 
 const Explore = () => {
     return (
-        <></>
+        <>
+            <Row style={{ paddingTop: '50px' }} className="container">
+                <Col sm={6}>
+                    <InputGroup>
+                        <Input />
+                        <InputGroupAddon addonType="append">
+                            <Button color="secondary">To the Right!</Button>
+                        </InputGroupAddon>
+                    </InputGroup>
+                </Col>
+            </Row>
+            <Row style={{ paddingTop: '50px', textAlign: 'left' }} className="container">
+                <Col sm={12}>
+                    <h2>Results</h2>
+                </Col>
+            </Row>
+        </>
     )
 }
 
