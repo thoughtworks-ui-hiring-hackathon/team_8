3b2f651bc46acb47baf984c448705da43743be3b
Ritesh Firodiya <firodiya.ritesh@confluxsys.com>
2019-06-21 17:27:35 +0530

    genere

--- src/scenes/Home/Home.js
+++ src/scenes/Home/Home.js
@@ -8,7 +8,12 @@ import { MoviesCarousel } from '../../components/MoviesCarousel';
 
 
 const getGenreString = (ids, map) => {
-    console.log(ids, map)
+    return ids.reduce((acc, crr) => {
+        if (acc) acc += `, ${crr}`;
+        else acc = crr;
+        return acc;
+
+    }, '')
 }
 
 const getLMbyConstrains = (movies = [], geners) => {
@@ -16,8 +21,8 @@ const getLMbyConstrains = (movies = [], geners) => {
     return movies.reduce((acc, crr) => {
         const { id, vote_count, vote_average, title, poster_path, genre_ids, release_date } = crr;
         acc.push({
-            id, vote_count, title, poster_path, genre_ids, release_date,
-            vote_average: getGenreString(vote_average, geners),
+            id, vote_count, title, poster_path, release_date,vote_average,
+            genre_ids: getGenreString(genre_ids, geners),
         });
         return acc;
     }, [])
@@ -29,8 +34,8 @@ const getTMbyConstrains = (movies = [], geners) => {
     return movies.reduce((acc, crr) => {
         const { id, vote_count, vote_average, title, poster_path, genre_ids, release_date } = crr;
         acc.push({
-            id, vote_count, title, poster_path, genre_ids, release_date,
-            vote_average: getGenreString(vote_average, geners),
+            id, vote_count, title, poster_path, release_date,vote_average,
+            genre_ids: getGenreString(genre_ids, geners),
         });
         return acc;
     }, [])
